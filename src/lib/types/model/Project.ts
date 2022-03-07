b13e93a21f09932cf8c78ebca96a5ee6abb63ef0
saudalmutawa <63466645+saudalmutawa@users.noreply.github.com>
2022-03-07 14:09:50 +0300

    Update Project.ts

--- src/lib/types/model/Project.ts
+++ src/lib/types/model/Project.ts
@@ -1,27 +1,39 @@
 import { ObjectId } from "bson";
-import { Document  } from "mongoose";
+import { Document } from "mongoose";
 
 export interface ProjectInfo extends Document {
-    name: string;
-    projectManager: ObjectId;
-    teamsId?:string[],
-    numberOfTasks?:number;
-    numberOfFinshedTasks?:number;
-    projectDeadline:Date;
-    startDate?:Date|number;
-    completedDate?:Date;
-    projectStatus?:'inProgress'|"deliver on time"|"late"|"deliver defore deadline"|"delivered after deadline";
-  }
+  name: string;
+  projectManager: ObjectId;
+  teamsId?: string[];
+  numberOfTasks?: number;
+  numberOfFinshedTasks?: number;
+  projectDeadline: Date;
+  startDate?: Date | number;
+  completedDate?: Date;
+  projectStatus?:
+    | "inProgress"
+    | "deliver on time"
+    | "late"
+    | "deliver defore deadline"
+    | "delivered after deadline";
+  clientId: ObjectId;
+}
 
-  export interface ProjectData{
-    id?:string;
-    name: string;
-    projectManager: string;
-    teamsId?:string[],
-    numberOfTasks?:number;
-    numberOfFinshedTasks?:number;
-    projectDeadline:Date;
-    startDate?:Date|number;
-    completedDate?:Date;
-    projectStatus?:'inProgress'|"deliver on time"|"late"|"deliver defore deadline"|"delivered after deadline";
-  }
+export interface ProjectData {
+  id?: string;
+  name: string;
+  projectManager: string;
+  teamsId?: string[];
+  numberOfTasks?: number;
+  numberOfFinshedTasks?: number;
+  projectDeadline: Date;
+  startDate?: Date | number;
+  completedDate?: Date;
+  projectStatus?:
+    | "inProgress"
+    | "deliver on time"
+    | "late"
+    | "deliver defore deadline"
+    | "delivered after deadline";
+  clientId: string;
+}
