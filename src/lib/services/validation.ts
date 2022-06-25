d40a7ed28ac9ec1a267a312eb1066a0716b03403
Zedan Saheer <zed.saheer5@gmail.com>
2022-06-25 12:40:02 +0300

    Fixing validation

--- src/lib/services/validation.ts
+++ src/lib/services/validation.ts
@@ -48,11 +48,11 @@ export const createTaskSchema = Joi.object({
     "string.min": "Project Id is required",
     "any.required": "Project Id is required",
   }),
-  name: Joi.string().required().min(4).max(20).messages({
+  name: Joi.string().required().min(6).max(40).messages({
     "string.base": "Task Name is required",
-    "string.empty": "Task name should be string with min 4 chars",
-    "string.min": "Task name length should be Min 4 chars",
-    "string.max": "Task name length should be Max 20 chars",
+    "string.empty": "Task name should be string with min 6 chars",
+    "string.min": "Task name length should be Min 6 chars",
+    "string.max": "Task name length should be Max 40 chars",
     "any.required": "Task Name is required",
   }),
   categoryId: Joi.string().required().messages({
@@ -113,11 +113,11 @@ export const editTaskSchema = Joi.object({
   id: Joi.string()
     .required()
     .messages({ "any.required": "Task id is required" }),
-  name: Joi.string().optional().min(4).max(20).messages({
+  name: Joi.string().optional().min(6).max(40).messages({
     "string.base": "Task Name is required",
-    "string.empty": "Task name should be string with min 4 chars",
-    "string.min": "Task name length should be Min 4 chars",
-    "string.max": "Task name length should be Max 20 chars",
+    "string.empty": "Task name should be string with min 6 chars",
+    "string.min": "Task name length should be Min 6 chars",
+    "string.max": "Task name length should be Max 40 chars",
   }),
   categoryId: Joi.string().optional().messages({
     "string.base": "Category is required",
