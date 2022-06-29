576e0245e6db205328da67662c1c4eac8c209388
Zedan Saheer <zed.saheer5@gmail.com>
2022-06-29 14:34:14 +0300

    Removing attachments

--- src/lib/services/mail/mail.ts
+++ src/lib/services/mail/mail.ts
@@ -48,15 +48,6 @@ const sendMail = async (data: Data) => {
       to: data.email,
       subject: `${data.subject}`,
       html: htmlToSend,
-      attachments: [{
-        filename: 'Logo.png',
-        path: __dirname + '/template/assets/images/Logo.png',
-        cid: 'logo'
-      },{
-        filename: 'lock.jpg',
-        path: __dirname + '/template/assets/images/lock.jpg',
-        cid: 'lock'
-      }],
       auth: {
         user: process.env.EMAIL_ADDRESS,
         refreshToken: process.env.REFRESH_TOKEN,
