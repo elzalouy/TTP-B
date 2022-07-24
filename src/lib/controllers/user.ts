e42d70de1298873cd82a634096b3752204e1e1e0
Zedan Saheer <zed.saheer5@gmail.com>
2022-07-24 15:49:01 +0530

    Fixing text for email template

--- src/lib/controllers/user.ts
+++ src/lib/controllers/user.ts
@@ -208,7 +208,7 @@ const UserController = class UserController extends UserDB {
         subject: "Update Password",
         token: token,
         path: "newPassword",
-        body: "Please set your new password using this link to start using your account",
+        body: "Set your new password",
       });
 
       return newUser;
