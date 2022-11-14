db1545e2337b21c60ccbb7f5b127f3a48b73c8e0
Ezat Elzalouy <ezat.elzalouy@o-projects.com>
2022-11-14 14:31:20 +0200

    Create Baord from TTP, and initialize the server with creating the main board

---
+++ src/lib/backgroundJobs/actions/department,actions.queue.ts
@@ -0,0 +1,39 @@
+import queue from "queue";
+import DepartmentController from "../../controllers/department";
+import TrelloActionsController from "../../controllers/trello";
+import Project from "../../models/Project";
+import { IDepartment } from "../../types/model/Department";
+import { ProjectInfo } from "../../types/model/Project";
+
+export const departmentsQueue = queue({ results: [], autostart: true });
+
+export const createProjectsCardsInCreativeBoard = (board: IDepartment) => {
+  let lists = board.lists;
+  departmentsQueue.push(async (cb) => {
+    let isProjectsList = lists.find((item) => item.name === "projects");
+    console.log({ isProjectsList });
+    if (isProjectsList) {
+      let projects = await Project.find({});
+      console.log({ projects });
+      projects.forEach(async (item: ProjectInfo) => {
+        if (item.cardId === null) {
+          let { id } = await TrelloActionsController.__createProject(
+            isProjectsList.listId,
+            {
+              name: item.name,
+              projectDeadline: item.projectDeadline,
+              startDate: item.startDate,
+            }
+          );
+          if (id) {
+            item.cardId = id;
+            item.boardId = board.boardId;
+            item.listId = isProjectsList.listId;
+            await item.save();
+          }
+        }
+      });
+    }
+    cb(null, true);
+  });
+};
