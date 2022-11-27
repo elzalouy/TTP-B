9b6b80a5d61ca3438d1f22264a0bb1faf0335269
Ezat Elzalouy <ezat.elzalouy@o-projects.com>
2022-11-27 13:50:57 +0200

    merge with main

--- src/index.ts
+++ src/index.ts
@@ -10,27 +10,36 @@ import cors from "cors";
 import routes from "./lib/startup/routes";
 import prod from "./lib/startup/prod";
 import cronJobs from "./lib/startup/cron";
+import logger from "./logger";
 const Config = require("config");
-//Express App
+
 const app = express();
 prod(app);
-app.use(express.json());
-app.use(cors());
-app.use(cookieParser());
-app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: false }));
-app.use(i18n.init);
-mongoDB();
-routes(app);
-config();
-let port = process.env.PORT || 5000;
 export const http = createServer(app);
 export const io = appSocket(http);
-http.listen(port, function () {
-  console.log("Welcome to", Config.get("name"));
-  console.log({
-    cards_webhook: Config.get("trelloWebhookUrlTask"),
-  });
-  console.log("server listen to port " + port);
-  cronJobs(io);
-});
+function server() {
+  try {
+    //Express App
+    app.use(express.json());
+    app.use(cors());
+    app.use(cookieParser());
+    app.use(morgan("dev"));
+    app.use(express.urlencoded({ extended: false }));
+    app.use(i18n.init);
+    mongoDB();
+    routes(app);
+    config();
+    let port = process.env.PORT || 5000;
+    http.listen(port, function () {
+      console.log("Welcome to", Config.get("name"));
+      console.log({
+        cards_webhook: Config.get("trelloWebhookUrlTask"),
+      });
+      console.log("server listen to port " + port);
+      cronJobs(io);
+    });
+  } catch (error) {
+    logger.error({ message: error });
+  }
+}
+server();
