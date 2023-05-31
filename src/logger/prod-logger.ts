3539ceacdb8a9de227257c5faf9ba0d234c861f3
Ezat Elzalouy <ezat.elzalouy@o-projects.com>
2023-05-31 10:21:20 +0200

    Fix logger file

--- src/logger/prod-logger.ts
+++ src/logger/prod-logger.ts
@@ -2,23 +2,23 @@ import { format, createLogger, transports } from "winston";
 const { combine, timestamp, errors, json, prettyPrint } = format;
 
 function buildProdLogger() {
-  console.log({ logger: `${__dirname}/src/logger/prod-logs/combined.log` });
+  console.log({ logger: `${__dirname}/prod-logs/combined.log` });
   return createLogger({
     format: combine(timestamp(), errors({ stack: true }), prettyPrint()),
     transports: [
       new transports.Console(),
       new transports.File({
-        filename: `${__dirname}/src/logger/prod-logs/error.log`,
+        filename: `${__dirname}/prod-logs/error.log`,
         level: "error",
         format: json(),
       }),
       new transports.File({
-        filename: `${__dirname}/src/logger/prod-logs/debug.log`,
+        filename: `${__dirname}/prod-logs/debug.log`,
         level: "debug",
         format: combine(prettyPrint()),
       }),
       new transports.File({
-        filename: `${__dirname}/src/logger/prod-logs/combined.log`,
+        filename: `${__dirname}/prod-logs/combined.log`,
         format: combine(prettyPrint()),
       }),
     ],
